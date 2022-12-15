0034788171e17912011de85fc40a63dec2e5b8da
sanjay <sanjayy7890@gmail.com>
2022-12-14 22:17:03 -0500

    styling updated

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var css = document.querySelector("h3");
+var css = document.querySelector(".current-gradient-info");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");

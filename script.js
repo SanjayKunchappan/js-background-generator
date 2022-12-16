6a2e4c94e2d1dde74bbaae88d987f3dd20cac7b9
sanjay <sanjayy7890@gmail.com>
2022-12-15 22:22:23 -0500

    script updated

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-var css = document.querySelector(".current-gradient-info");
+var cssText = document.querySelector(".current-gradient-info");
 var color1 = document.querySelector(".color1");
 var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
 function setGradient() {
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ")";
-
-	css.textContent = body.style.background + ";";
+	//background: linear-gradient(to right, #780206 , #061161); css format for gradient
+	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+	cssText.textContent = body.style.background + ";";
 }
 
-color1.addEventListener("input", setGradient);
-
+color1.addEventListener("input", setGradient);// event listener for input
 color2.addEventListener("input", setGradient);
